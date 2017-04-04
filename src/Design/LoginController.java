7386393a60c738b138729664f439075af378004c
Andrei Merfu <Andrei Merfu>
2017-04-04 21:57:16 +0300

    Initial commit

---
+++ src/Design/LoginController.java
@@ -0,0 +1,36 @@
+package Design;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.io.IOException;
+
+public class LoginController {
+    @FXML private Button singIn;
+    @FXML private Button useToken;
+    @FXML private TextField username;
+
+    @FXML private void handleButtonAction(){
+        username.setText("I love JavaFX");
+    }
+
+    @FXML private void handleUseToken(ActionEvent event) throws IOException{
+        if(event.getSource()==useToken) {
+            Parent root = FXMLLoader.load(getClass().getResource("UseToken.fxml"));
+            Scene scene = new Scene(root,600,400);
+            Stage stage=(Stage) useToken.getScene().getWindow();
+            stage.setTitle("Chestionare Auto categoria B");
+            stage.setScene(scene);
+
+            stage.show();
+        }
+    }
+
+}
---
+++ src/Design/UseTokenController.java
@@ -0,0 +1,35 @@
+package Design;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class UseTokenController {
+    @FXML private Button backButton;
+    @FXML private Button useToken;
+
+    @FXML private Label MessageText;
+
+    @FXML private void handleTokenAction(){
+        MessageText.setText("Your token is incorrect, please try again.");
+    }
+
+    @FXML private void handleBackButton(ActionEvent event) throws IOException{
+            if(event.getSource()==backButton) {
+            Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+            Scene scene = new Scene(root,600,400);
+            Stage stage=(Stage) useToken.getScene().getWindow();
+            stage.setTitle("Chestionare Auto categoria B");
+            stage.setScene(scene);
+
+            stage.show();
+        }
+    }
+}
